58cf07ebb4be1e8198b5451a62d1941db5eef957
ThatGirlWhoCodes <Dimperio@colum.edu>
2025-03-05 18:13:47 -0600

    back

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <!-- MySQL connection -->
 <?php
-    $servername = 'localhost';
-    $username = 'root';
-    $password = 'g!g0rigin@1s!';
-    $dbname = 'Chatroom';
-    $server = '455-application-design';
-// if (isset($_REQUEST["send message"])) {
-//     $url = 'localhost';
-// $username = 'root';
-// $password = 'g!g0rigin@1s!';
-// $dbname = 'Chatroom';
-// $server = '455-application-design';
+if (isset($_REQUEST["send message"])) {
+    $url = 'localhost';
+$username = 'root';
+$password = 'g!g0rigin@1s!';
+$dbname = 'Chatroom';
+$server = '455-application-design';
 
 //create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -19,21 +14,6 @@ if ($conn->connect_error) {
     die('Database Connection Failed: '.$conn->connect_error);
 }
 echo'Connected Successfully!';
-
-//handle message submission
-if ($_SERVER["REQUEST_METHOD"] == "POST" && ISSET($_POST["send"])){
-    $name = $conn->real_escape_string($_POST["name"]);
-    $message = $conn->real_escape_string($_POST["message"]);
-
-    if (!empty($name) && !empty($message)) {
-        $query = "INSERT INTO messages (name, message) VALUES ('$name', '$message')";
-        $conn->query($query);
-    }
-
-    //refresh page with new message
-    header("Location: index.php");
-    exit();
-}
 
 $query = 'SELECT name, message, date_epoch FROM messages ORDER BY date_epoch DESC';
 $result = mysqli_query($conn, $query);
@@ -53,7 +33,7 @@ while ($row = mysqli_fetch_array($result)) {
 }
 
 $conn-> close();
-//}
+}
 ?>
 
 <!DOCTYPE html>
@@ -68,7 +48,7 @@ $conn-> close();
 <body>
 
 </body> 
-    <h1>Welcome to Chat!</h1>
+
     <div class="chat-box">
     <!-- display message user sent -->
     <?php while ($row = $result->fetch_assoc()): ?>
@@ -79,15 +59,15 @@ $conn-> close();
             </div>
             <!-- loop until no more messages -->
         <?php endwhile; ?>
-            </div>
     </div>    
 
-    <form method="POST" action="">
-    <input type="text" name="name" placeholder="Enter your name" required>
+    <form method="POST" action="#"> 
+            <input type="text" class="form-control" id="source-edit" placeholder="Enter Source" required >
     <input type="text" name="message" placeholder="Type your message..." required>
-    <button type="submit" name="send">Send</button>
-</form>
-
+    <button type="button">
+        Type message here
+    </button>
+    </form>
 </body>
 
 </html> 
